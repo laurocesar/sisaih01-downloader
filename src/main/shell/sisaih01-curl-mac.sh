3c1f7ddeacf219ded1e1ee671c9e9335ac978bc0
laurocesar <laurocesar@gmail.com>
2021-11-19 10:12:36 -0300

    adiciona sed

--- src/main/shell/sisaih01-curl-mac.sh
+++ src/main/shell/sisaih01-curl-mac.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -s --insecure ftp://ftp2.datasus.gov.br/public/sistemas/dsweb/SIHD/Programas/sisaih01_leiame.txt | iconv -f ISO-8859-1 -t UTF8-MAC | sed -n '/----------------------------------------------------/q;p' 
+curl -s --insecure ftp://ftp2.datasus.gov.br/public/sistemas/dsweb/SIHD/Programas/sisaih01_leiame.txt | iconv -f ISO-8859-1 -t UTF8-MAC | sed -n '/----------------------------------------------------/q;p' | sed -n -e '/========/,$p'
